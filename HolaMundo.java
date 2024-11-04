aaf15326b8fa692f62bab86f4c3c1b27f319be24
mangompae <mgomezpaez280705@gmail.com>
2024-11-04 17:48:35 +0100

    Comit2

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ class HolaMundo{
     public static void main(String[] args) {
         String rojo="\033[32m";
         String azul="\033[34m";
-        System.out.println("Hoal mundi");
+        System.out.println("Hoal mundaco");
 }
 }
